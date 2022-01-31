89ea350fb876cce23b16f734213ef28c451ee6d5
Horvi <48880572+HKrisz@users.noreply.github.com>
2022-01-31 21:41:14 +0100

    Add files via upload

---
+++ fibonacci_rekurzív/fibonacci_rekurzív/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_rekurzív
+{
+    class Program
+    {
+        static int fibonacci(int n)
+        {
+            if ( n==0)
+            {
+                return 0;
+            }
+            else if (n==1)
+            {
+                return 1;
+
+            }
+            else
+            {
+                return fibonacci(n - 2) + fibonacci(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci(5));
+
+            Console.ReadKey();
+        }
+    }
+}
